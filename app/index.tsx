c36a61d148a915fff64466d5442096e197e636f5
Uwakmfon1 <udohuwakmfon2@gmail.com>
2026-01-08 15:25:54 +0100

    styling config modified

--- app/index.tsx
+++ app/index.tsx
@@ -3,13 +3,13 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Welcome to the homepage</Text>
+      // style={{
+      //   flex: 1,
+      //   justifyContent: "center",
+      //   alignItems: "center",
+      // }}
+        className="w-full h-full flex justify-center align-center bg-red-700">
+      <Text className="text-blue-600 font-bold text-lg bg-red-400">Welcome Home</Text>
     </View>
   );
 }
